a8e3ec5bb196c67e133e0b994c18566cb10793c9
Harut Norikyan <harutnoro@mail.ru>
2025-09-10 12:16:16 +0400

    changes

--- app/layout.js
+++ app/layout.js
@@ -67,13 +67,79 @@ export const metadata = {
     shortcut: "/favicon.ico",
     apple: "/apple-touch-icon.png",
   },
+  alternates: {
+    types: {
+      'application/xml': '/sitemap.xml',
+    },
+  },
 };
+
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//       <Head>
+//         {/* <link rel="sitemap" type="application/xml" title="Sitemap" href="/sitemap.xml" /> */}
+//         <Script
+//           src="https://www.googletagmanager.com/gtag/js?id=G-8DFQECXXJZ"
+//           strategy="afterInteractive"
+//         />
+//         <Script id="google-analytics" strategy="afterInteractive">
+//           {`
+//             window.dataLayer = window.dataLayer || [];
+//             function gtag(){dataLayer.push(arguments);}
+//             gtag('js', new Date());
+//             gtag('config', 'G-8DFQECXXJZ');
+//           `}
+//         </Script>
+//         <Script id="yandex-metrika" strategy="afterInteractive">
+//           {`
+//             (function(m,e,t,r,i,k,a){
+//                 m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+//                 m[i].l=1*new Date();
+//                 for (var j = 0; j < document.scripts.length; j++) {
+//                   if (document.scripts[j].src === r) { return; }
+//                 }
+//                 k=e.createElement(t),a=e.getElementsByTagName(t)[0],
+//                 k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+//             })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=104079367', 'ym');
+
+//             ym(104079367, 'init', {
+//               ssr:true,
+//               webvisor:true,
+//               clickmap:true,
+//               ecommerce:"dataLayer",
+//               accurateTrackBounce:true,
+//               trackLinks:true
+//             });
+//           `}
+//         </Script>
+//         <noscript>
+//           <div>
+//             <img src="https://mc.yandex.ru/watch/104079367" style={{ position: "absolute", left: "-9999px;" }} alt="" />
+//           </div>
+//         </noscript>
+
+
+//       </Head>
+//       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+//         <Header />
+//         {children}
+//         <Footer />
+//       </body>
+//     </html>
+//   );
+// }
+
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="sitemap" type="application/xml" title="Sitemap" href="/sitemap.xml" />
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Header />
+        {children}
+        <Footer />
+
+        {/* Google Analytics */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-8DFQECXXJZ"
           strategy="afterInteractive"
@@ -83,22 +149,24 @@ export default function RootLayout({ children }) {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-8DFQECXXJZ');
+            gtag('config', 'G-8DFQECXXJZ', { page_path: window.location.pathname });
           `}
         </Script>
+
+        {/* Yandex Metrika */}
         <Script id="yandex-metrika" strategy="afterInteractive">
           {`
             (function(m,e,t,r,i,k,a){
-                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                m[i].l=1*new Date();
-                for (var j = 0; j < document.scripts.length; j++) {
-                  if (document.scripts[j].src === r) { return; }
-                }
-                k=e.createElement(t),a=e.getElementsByTagName(t)[0],
-                k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=104079367', 'ym');
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for(var j=0;j<document.scripts.length;j++){
+                if(document.scripts[j].src===r){return;}
+              }
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0];
+              k.async=1;k.src=r;a.parentNode.insertBefore(k,a)
+            })(window,document,'script','https://mc.yandex.ru/metrika/tag.js?id=104079367','ym');
 
-            ym(104079367, 'init', {
+            ym(104079367,'init',{
               ssr:true,
               webvisor:true,
               clickmap:true,
@@ -110,16 +178,9 @@ export default function RootLayout({ children }) {
         </Script>
         <noscript>
           <div>
-            <img src="https://mc.yandex.ru/watch/104079367" style={{ position: "absolute", left: "-9999px;" }} alt="" />
+            <img src="https://mc.yandex.ru/watch/104079367" style={{ position: "absolute", left: "-9999px" }} alt="" />
           </div>
         </noscript>
-
-
-      </Head>
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <Header />
-        {children}
-        <Footer />
       </body>
     </html>
   );
