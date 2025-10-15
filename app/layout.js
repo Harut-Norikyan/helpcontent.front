97a336fa78553e3ca0b93c6c7204379ef5b828c8
Harut Norikyan <harutnoro@mail.ru>
2025-10-15 11:40:35 +0400

    changesss

--- app/layout.js
+++ app/layout.js
@@ -77,11 +77,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+
       <head>
         <meta name="google-adsense-account" content="ca-pub-9929665031663141" />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9929665031663141"
           crossorigin="anonymous"></script>
       </head>
+
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Header />
         {children}
